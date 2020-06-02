64567e2bd89d868e66a172e26e9e4f58e1899d42
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 16:56:35 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "IAM",
-  products: [
-      .library(name: "IAM", targets: ["IAM"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "IAM", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "IAM",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "IAM", targets: ["IAM"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "IAM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/IAM"),
+    ]
 )
